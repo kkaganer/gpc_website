69f36b05329f0bab1c4b769063d8015d3dc5d7dc
kknavigator <kristina.kaganer@navigator.tech>
2026-03-31 18:33:29 +0100

    new map

--- src/components/whatson/EventMap.jsx
+++ src/components/whatson/EventMap.jsx
@@ -62,8 +62,10 @@ export default function EventMap({ events, activeEventId, onMarkerClick, flyTo }
       scrollWheelZoom={true}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        subdomains="abcd"
+        maxZoom={20}
       />
 
       {flyTo && <FlyToEvent lat={flyTo.lat} lng={flyTo.lng} />}
